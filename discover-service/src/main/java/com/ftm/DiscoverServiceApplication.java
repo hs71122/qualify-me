db4d7dcdd552ea0058134cc978fe464bb4cc4f9d
Harshana Samaranayake <awharshana@gmail.com>
2017-07-08 23:35:14 +0530

    initial commit

---
+++ discover-service/src/main/java/com/ftm/DiscoverServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ftm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverServiceApplication.class, args);
+	}
+}
