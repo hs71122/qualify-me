4fc3a1427baa1197022e1d73d10a540a6190101c
Harshana Samaranayake <awharshana@gmail.com>
2017-07-09 21:41:08 +0530

    service discovery server initial commit

--- discover-service/src/main/java/com/ftm/DiscoverServiceApplication.java
+++ discover-service/src/main/java/com/ftm/DiscoverServiceApplication.java
@@ -2,7 +2,9 @@ package com.ftm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoverServiceApplication {
 
